a0fa8ff8eb6adf061f05d508297d13136e4cdb31
laon <laon198@gmail.com>
2020-12-03 14:59:38 +0900

    storage config

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,1 +1,8 @@
 from .common import *
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+# FIXME : 9:18
